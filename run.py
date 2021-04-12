bb0ba0ed28bd3f1ec54076c654c40aaaceead31f
Bazileus <baz1leus@mail.ru>
2021-04-12 08:35:23 +0300

    api v2

--- run.py
+++ run.py
@@ -1,78 +1,100 @@
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
-stores = [
+items = [
     {
-        'name': 'My Store',
-        'items': [
-                {
-                'name': 'chair',
-                'price': 1500,
-            }
-        ]
-    }
+        'name': 'chairs',
+        'price': 1500,
+    },
+    {
+        'name': 'chair-2',
+        'price': 999,
+    },
+    {
+        'name': 'chair-3',
+        'price': 1999,
+    },
 ]
 
-# POST /store -> name
-@app.route('/store', methods=['POST'])
-def create_store():
-    data = request.get_json()
-    store_name = data['name']
-    
-    for store in stores:
-        if store['name'] == store_name:
-            return jsonify({'message': 'store already exists'})
-    
-    store = {
-        'name': store_name,
-        'items': [],
-    }
-    stores.append(store)
-    
-    return jsonify(store)
-
-# POST /store/<name>/item -> name, price
-@app.route('/store/<string:store_name>/item', methods=['POST'])
-def create_item(store_name):
-    data = request.get_json()
-    
-    for store in stores:
-        if store['name'] == store_name:
-            item = {
-                'name': data['name'],
-                'price': data['price'],
-            }
-            store['items'].append(item)
-            return jsonify(item)
-    
-    return jsonify({'message': 'store not found'})
-
-# GET /store
-@app.route('/store')
-def get_stores():
-    return jsonify(stores)
-    
-
-# GET /store/<name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    
-    return jsonify({'message': 'store not found'})
-
-# GET /store/<name>/item
-@app.route('/store/<string:name>/item')
-def get_items(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store['items'])
-    
-    return jsonify({'message': 'store not found'})
+parser = reqparse.RequestParser()
 
 
+class Item(Resource):
+    # GET /items/<name>
+    def get(self, name):
+        _item = list(filter(lambda x: x['name'] == name, items))
+        if _item:
+            return {'searched_item': _item[0]}
+        return {'message': 'not found'}
+    
+    # POST /items/<name>
+    def post(self, name):
+        parser.add_argument('price')
+        _item = list(filter(lambda x: x['name'] == name, items))
+        if _item:
+            return {'message': 'item already exists'}
+        if parser.parse_args()['price'].isdigit():
+            items.append({'name': name, 'price': int(parser.parse_args()['price'])})
+            return {'items': items}
+        else:
+            return {'message': 'price should be an integer'}
+    
+    # PUT /items/<name>
+    def put(self, name):
+        parser.add_argument('price')
+        _item = list(filter(lambda x: x['name'] == name, items))
+        if _item:
+            if parser.parse_args()['price'].isdigit():
+                _item[0]['price'] = int(parser.parse_args()['price'])
+                return {'items': items}
+            else:
+                return {'message': 'price should be an integer'}
+        if parser.parse_args()['price'].isdigit():
+            items.append({'name': name, 'price': int(parser.parse_args()['price'])})
+            return {'items': items}
+        else:
+            return {'message': 'price should be an integer'}
+    
+    # DELETE /items/<name>
+    def delete(self, name):
+        _item = list(filter(lambda x: x['name'] == name, items))
+        if _item:
+            items.remove(_item[0])
+            return {'items': items}
+        return {'message': 'not found'}
+
+
+api.add_resource(Item, '/items/<name>')
+
+
+class ItemList(Resource):
+    
+    # GET /items
+    def get(self):
+        return {'items': items}
+    
+    
+    # POST /items
+    def post(self):
+        parser.add_argument('itemlist', type=dict, action='append')
+        _itemlist = parser.parse_args()['itemlist']
+        
+        errors = []
+        for item in _itemlist:
+            _item = list(filter(lambda x: x['name'] == item['name'], items))
+            if _item:
+                errors.append(item['name']+' already exists')
+            else:
+                if isinstance(item['price'], int):
+                    items.append({'name': item['name'], 'price': item['price']})
+                else:
+                    errors.append(item['name']+' price should be an integer')
+        return {'errors': errors, 'items': items}
+
+api.add_resource(ItemList, '/items')
 
 if __name__ == '__main__':
     app.run(debug=True)
